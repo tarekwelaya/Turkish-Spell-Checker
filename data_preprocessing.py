1a4155c2a6b60aa0305eeed6cb5bbd5380908ded
Tarek Welaya <tarek.welaya@gmail.com>
2020-05-29 03:45:09 +0300

    finalizing model

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,42 +1,141 @@
 import csv
 import re
-            
-def process_data():
-    with open('wiki_00') as file, open('wiki_tr.csv', 'w') as csvfile:
-        lines_num = 0
-        lines =[]
-        title = False
-        lines_num = 0
-        for line in file:
-            if re.match('<[^<]+>',line):
-                title = True
-                continue
-            if title:
-                title = False
-                continue
-            line = re.sub('[\(\[].*?[\)\]]', '', line)
-            line = line.rstrip("\n")
-            line = re.sub(r'\d+','',line)
-            line = re.sub(r"\s+", " ", line)
-            pattern = re.compile("^[a-zA-Z0-9ğüşöçıİĞÜŞÖÇ \n]+$")
-            if not pattern.match(line):
-                continue
-            if len(line) < 25:
-                continue
-            if not line.strip():
-                continue
-            if line in  lines:
-                continue
-            if lines_num == 1000:
-                break
-            lines_num += 1
-            lines.append(line)
-            lines = sorted(lines, key=len)
-        writer = csv.writer(csvfile, delimiter='|')
-        writer.writerow(('ID', 'Line'))
-        for idx, line in enumerate(lines, 1):
-            writer.writerow((idx, line))
+import math
+import random
+import numpy as np
+
+
+
+
+class DataProcessing():
+    
+    def __init__(self):
+        self.keyboard_cartesian = {'q': {'x': 1, 'y': 1}, 'w': {'x': 2, 'y': 1}, 'e': {'x': 3, 'y': 1},
+                                   'r': {'x': 4, 'y': 1}, 't': {'x': 5, 'y': 1}, 'y': {'x': 6, 'y': 1},
+                                   'u': {'x': 7, 'y': 1}, 'ı': {'x': 8, 'y': 1}, 'o': {'x': 9, 'y': 1},
+                                   'p': {'x':10, 'y': 1}, 'ğ': {'x':11, 'y': 1}, 'ü': {'x':12, 'y': 1},
+                                   'a': {'x': 1, 'y': 2}, 's': {'x': 2, 'y': 2}, 'd': {'x': 3, 'y': 2},
+                                   'f': {'x': 4, 'y': 2}, 'g': {'x': 5, 'y': 2}, 'h': {'x': 6, 'y': 2},
+                                   'j': {'x': 7, 'y': 2}, 'k': {'x': 8, 'y': 2}, 'l': {'x': 9, 'y': 2},
+                                   'ş': {'x':10, 'y': 2}, 'i': {'x':11, 'y': 2},
+                                   'z': {'x': 1, 'y': 3}, 'x': {'x': 2, 'y': 3}, 'c': {'x': 3, 'y': 3},
+                                   'v': {'x': 4, 'y': 3}, 'b': {'x': 5, 'y': 3}, 'n': {'x': 6, 'y': 3},
+                                   'm': {'x': 7, 'y': 3}, 'ö': {'x': 8, 'y': 3}, 'ç': {'x': 9, 'y': 3}}
+        
+        self.nearest_to_i = self.get_nearest_to_i()
+        self.lines = []
+        self.corrupted_lines = []
+
+    def get_nearest_to_i(self):
+        nearest_to_i = {}
+        for i in self.keyboard_cartesian.keys():
+            nearest_to_i[i] = []
+            for j in self.keyboard_cartesian.keys():
+                distance = self.euclidean_distance(i, j) 
+                if distance < 2.0:
+                    nearest_to_i[i].append(j)
+        return nearest_to_i
+
+
+    def euclidean_distance(self, a, b):
+        X = (self.keyboard_cartesian[a]['x'] - self.keyboard_cartesian[b]['x']) ** 2      
+        Y = (self.keyboard_cartesian[a]['y'] - self.keyboard_cartesian[b]['y']) ** 2
+        return math.sqrt(X + Y)
+        
+    def convert_data_to_lines(self, filename, max_chars=100, min_words =8, min_char =15):
+        pattern = re.compile("^[a-zA-Z0-9ğüşöçıİĞÜŞÖÇ \n]+$")
+        with open(filename, "r") as file:
+            lines_num = 0
+            lines =[]
+            title = False
+            lines_num = 0
+            for line in file:
+                if re.match('<[^<]+>',line):
+                    title = True
+                    continue
+                if title:
+                    title = False
+                    continue
+                line = re.sub('[\(\[].*?[\)\]]', '', line)
+                line = line.rstrip("\n")
+                line = re.sub(r'\d+','',line)
+                line = re.sub(r"\s+", " ", line)
+                if not pattern.match(line):
+                    continue
+                lines_num += 1
+                line = re.sub('I','ı',line) # convert I to lower case ı
+                line = re.sub('İ','i',line)
+                line = line.lower()
+                line = line.lstrip() # remove white-sapace if exsist in the begining of the string
+                line = line.strip() #remove white-space at the end
+                if not line.strip():
+                    continue
+                if len(line.split()) < min_words-1 and len(line) < min_char:
+                    continue
+                if len(line) > max_chars:
+                    sub_lines = []
+                    while len(line) >= max_chars:
+                        ar = self.find_space(line, ' ')
+                        index = 0
+                        last_space = ar[index];
+                        while 1:
+                            index = index + 1
+                            if len(ar) < index+1:
+                                break
+                            if ar[index] >= max_chars:
+                                break
+                            last_space = ar[index]
+                        sub_lines.append(line[:last_space])
+                        line = line[last_space+1:]
+                    for sub in sub_lines:
+                        lines.append(sub)
+                    if len(line.split()) > min_words-1 and len(line) > min_char:
+                        lines.append(line)
+                else:
+                    lines.append(line)
+                    
+                lines = list(set(lines))
+                lines = sorted(lines, key=len)
+                
+                self.lines = lines
+
+    def find_space(self, s, ch):
+        return [i for i, ltr in enumerate(s) if ltr == ch]
+
+    def corrupt_lines(self, threshold = 7):
+        corrupted_lines = []
+        for line in self.lines:
+            for x in range(random.randint(1, 10)):
+                corrupt_arr = np.random.randint(100, size=(len(line),))
+                li = list(line)
+                cor = corrupt_arr < threshold
+                for i, corrupt in enumerate(cor):
+                    if corrupt and li[i] != ' ':
+                        corrupt_method = np.random.randint(1,4)
+                        if corrupt_method == 1:
+                            li[i] = random.choice(self.nearest_to_i[li[i]])
+                        elif corrupt_method == 2:
+                            li[i] = ''
+                        elif corrupt_method == 3:
+                            li.insert(i,random.choice(self.nearest_to_i[li[i]]))
+                corrupted_lines.append((''.join(li), line))
+        self.corrupted_lines = corrupted_lines
+
+    def write_data(self, outfname):
+        with open(outfname, 'w') as f:
+            for c, l in self.corrupted_lines:
+                f.write(c + "|" +l + "\n")
 
 if __name__ == "__main__":
-    process_data()
-
+    process = DataProcessing()
+    process.convert_data_to_lines("wiki_train")
+    process.corrupt_lines() #max_type_num
+    process.write_data("train.txt")
+    
+    process.convert_data_to_lines("wiki_test")
+    process.corrupt_lines() #max_type_num
+    process.write_data("test.txt")
+    
+    process.convert_data_to_lines("wiki_val")
+    process.corrupt_lines() #max_type_num
+    process.write_data("val.txt")
---
+++ model.py
@@ -0,0 +1,288 @@
+from io import open
+import torch
+import torch.nn as nn
+from torch import optim
+
+import string
+import random
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=True)
+        
+    def forward(self, input):
+        embedded = self.embedding(input)
+        output, hidden = self.lstm(embedded, self.initHidden(input.size(0)))
+        return hidden
+
+    def initHidden(self, batchsize):
+        return (torch.zeros(1, batchsize, self.hidden_size, device=device), 
+                torch.zeros(1, batchsize, self.hidden_size, device=device))
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input)
+        output, hidden = self.lstm(output, hidden)
+        output = self.out(output).permute(0, 2, 1)
+        return output, hidden
+
+#class AttnDecoderRNN(nn.Module):
+#    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+#        super(AttnDecoderRNN, self).__init__()
+#        self.hidden_size = hidden_size
+#        self.output_size = output_size
+#        self.dropout_p = dropout_p
+#        self.max_length = max_length
+#
+#        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+#        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+#        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+#        self.dropout = nn.Dropout(self.dropout_p)
+#        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size)
+#        self.out = nn.Linear(self.hidden_size, self.output_size)
+#
+#    def forward(self, input, hidden, encoder_outputs):
+#        embedded = self.embedding(input).view(1, 1, -1)
+#        embedded = self.dropout(embedded)
+#
+#        attn_weights = F.softmax(
+#            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+#        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+#                                 encoder_outputs.unsqueeze(0))
+#
+#        output = torch.cat((embedded[0], attn_applied[0]), 1)
+#        output = self.attn_combine(output).unsqueeze(0)
+#
+#        output = F.relu(output)
+#        output, hidden = self.lstm(output, hidden)
+#
+#        output = F.log_softmax(self.out(output[0]), dim=1)
+#        return output, hidden, attn_weights
+#
+#    def initHidden(self):
+#        return torch.zeros(1, 1, self.hidden_size, device=device)
+    
+class SpellChecker(object):
+    def __init__(self, encoder, decoder, i2c, c2i):
+        self.encoder = encoder
+        self.decoder = decoder
+        self.i2c = i2c
+        self.c2i = c2i
+        
+    def check(self, line, max_sub_len=2):
+        max_target_length = len(line) + max_sub_len
+        decoder_input = torch.LongTensor([[EOS_TOKEN]])
+        encode = encode_lines([line])
+        output = []
+        with torch.no_grad():
+            decoder_hidden = self.encoder(encode)
+            for di in range(max_target_length):
+                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                decoder_input = decoder_output.argmax(dim=1)
+
+                if decoder_input.item() == c2i[EOS_TOKEN]:
+                    break
+                else:
+                    output.append(decoder_input.item())
+        return "".join([ i2c[i] for i in output ])
+
+    def evaluate(self):
+        return
+
+
+SOS_TOKEN = 1
+EOS_TOKEN = 0
+BATCH = 8
+chars = string.ascii_lowercase + "ıüşğçö "
+
+c2i = {c :i for i,c in enumerate(chars)}
+c2i[EOS_TOKEN] = len(c2i) + 1
+c2i[SOS_TOKEN] = len(c2i) + 1
+i2c = list(chars) 
+
+def encode_lines(lines, prepad=True): 
+    encoded_lines = [] 
+    max_length = max(len(l) for l in lines) 
+    for l in lines:
+        if prepad:
+            encoded_lines.append([c2i[SOS_TOKEN]]*(max_length - len(l)) + [c2i[c] for c in l] + [c2i[EOS_TOKEN]]) 
+        else:
+            encoded_lines.append([c2i[c] for c in l] + [c2i[EOS_TOKEN]] * (max_length - len(l)))
+    return torch.LongTensor(encoded_lines)     
+
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+def get_batched_pairs(fileName):
+    
+    lines = open(fileName).read().strip().split('\n') 
+    pairs = [[s for s in l.split('|')] for l in lines]
+    for pairs in batch(pairs, n=BATCH):
+        in_lines, output_lines = zip(*pairs)
+        yield encode_lines(in_lines), encode_lines(output_lines)
+    
+    return
+
+def get_sets():
+    training_pairs = list(get_batched_pairs('train.txt'))
+    validation_pairs = list(get_batched_pairs('val.txt'))
+    return training_pairs, validation_pairs
+
+
+
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, 
+          criterion,teacher_forcing_ratio = 0.5, update=True ):
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    target_length = target_tensor.size(1)
+
+    loss = 0
+    decoder_hidden = encoder(input_tensor)
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        decoder_output, decoder_hidden = decoder(target_tensor, decoder_hidden)
+        loss = criterion(decoder_output, target_tensor)
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        decoder_input = torch.LongTensor([[EOS_TOKEN] for i in range(target_tensor.size(0)) ])
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            decoder_input = decoder_output.argmax(dim=1)
+
+            loss += criterion(decoder_output, target_tensor[:, di].unsqueeze(-1))
+            if decoder_input.sum() == EOS_TOKEN:
+                break
+    if update:
+        loss.backward()
+        
+        encoder_optimizer.step()
+        decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+    
+import time
+import math
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+def trainIters(encoder, decoder,training_pairs, validation_pairs, sc, epochs=10, dev_line="ben geldim", print_every=100, plot_every=100, learning_rate=0.001):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    report_val = len(training_pairs)
+    training_pairs = [training_pairs for i in range(epochs)]
+    for l in training_pairs: random.shuffle(l)
+    training_pairs = sum(training_pairs, [])
+    criterion = nn.CrossEntropyLoss()
+    n_iters = len(training_pairs)
+    bestloss = 1e3
+    print(report_val)
+    for iter, training_pair in enumerate(training_pairs, start=1):
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+        loss = train(input_tensor, target_tensor, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion, teacher_forcing_ratio=(1 - iter/len(training_pairs) ))
+        if iter % report_val == 0:
+            encoder.eval()
+            decoder.eval()
+            val_loss = 0
+            for _iter, validation_pair in enumerate(validation_pairs, start = 1):
+                input_tensor = validation_pair[0]
+                target_tensor = validation_pair[1]
+                val_loss += train(input_tensor, target_tensor, encoder, 
+                                 decoder, encoder_optimizer, decoder_optimizer, teacher_forcing_ratio=-1, criterion, update=False)
+            if val_loss < bestloss:
+                bestloss = val_loss
+            else:
+                print('bigger than best loss')
+                return
+            
+            encoder.train()
+            decoder.train()
+        
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+            print(sc.check(dev_line))
+
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+    showPlot(plot_losses)
+
+
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+import matplotlib.ticker as ticker
+import numpy as np
+
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+
+hidden_size = 32
+encoder1 = EncoderRNN(len(c2i) + 1, hidden_size).to(device)
+decoder1 = DecoderRNN(hidden_size, len(c2i) + 1).to(device)
+sc = SpellChecker(encoder1, decoder1, i2c, c2i)
+training_pairs, validation_pairs, = get_sets()
+trainIters(encoder1, decoder1, training_pairs, validation_pairs ,sc, print_every=100)
+
+# TODOs
+# - Split data into train/dev/test
+# - Calculate dev loss without calling loss.backward to know when to stop training
+# - Write function to decode step by step to test the model
+
